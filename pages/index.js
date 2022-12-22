4f261e42fd52bb59ce01e697b44f1e737d7b7e9e
Henry Smith <henry.smith@netlify.com>
2022-12-22 16:44:37 +0000

    Fix escape

--- pages/index.js
+++ pages/index.js
@@ -287,7 +287,7 @@ const Landing = () => {
                 onClick={handleSubmit}
                 className="inline-flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
               >
-                Let's Go
+                Lets Go
               </button>
               {formError && (
                 <div className="text-red-400 text-center pt-4 text-lg">
