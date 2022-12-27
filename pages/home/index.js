72643b5d55b97db977028672f01a46f383dd27d4
Henry Smith <henry.smith@netlify.com>
2022-12-27 23:53:12 +0000

    Change product layout

--- pages/home/index.js
+++ pages/home/index.js
@@ -101,27 +101,34 @@ const Home = ({ products, posts, message }) => {
         </div>
       </div>
 
-      <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-        <h2 id="products-heading" className="sr-only">
-          Suggest Products
-        </h2>
-
-        <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
-          {hydratedProducts.map((product) => (
-            <a key={product?.name} href={product?.url} className="group">
-              <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg sm:aspect-w-2 sm:aspect-h-3">
-                <img
-                  src={product?.largeImage}
-                  alt="Not Found"
-                  className="h-full w-full object-cover object-center group-hover:opacity-75"
-                />
-              </div>
-              <div className="mt-4 flex items-center justify-between text-base font-medium text-gray-900">
-                <h3>{product?.title}</h3>
-                <p>{`$${product?.price?.salePrice}`}</p>
-              </div>
-            </a>
-          ))}
+    
+        <div className="mx-auto max-w-7xl overflow-hidden py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+          <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 lg:gap-x-8">
+            {hydratedProducts.map((product) => (
+              <a
+                key={product?.name}
+                href={product?.url}
+                className="group text-sm"
+              >
+                <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-100 group-hover:opacity-75">
+                  <img
+                    src={product.largeImage}
+                    alt="Not Found"
+                    className="h-full w-full object-cover object-center"
+                  />
+                </div>
+                <h3 className="mt-4 font-medium text-gray-900">
+                  {product.title}
+                </h3>
+                <p className="italic text-gray-500">
+                  {product.shortDescription}
+                </p>
+                <p className="mt-2 font-medium text-gray-900">
+                  {`$${product?.salePrice}`}
+                </p>
+              </a>
+            ))}
+          </div>
         </div>
       </div>
     </div>
