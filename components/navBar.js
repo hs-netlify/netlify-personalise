f557465887351c864fc8809770ba488b5d978188
Henry Smith <henry.smith@netlify.com>
2022-12-29 18:03:51 +0000

    Improve home page styling

--- components/navBar.js
+++ components/navBar.js
@@ -17,7 +17,7 @@ export default function NavBar() {
     <Disclosure as="nav" className="bg-white shadow">
       {({ open }) => (
         <>
-          <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          <div className="mx-auto max-w-7xl px-4  sm:px-6 lg:px-8">
             <div className="flex h-16 justify-between">
               <div className="flex">
                 <div className="flex flex-shrink-0 items-center cursor-pointer w-36">
--- pages/home/index.js
+++ pages/home/index.js
@@ -47,7 +47,7 @@ const Home = ({ products, posts, message, currency }) => {
 
   return (
     <div id="main-body" className="min-h-screen h-full">
-      <NavBar />
+      <NavBar className="z-10" />
       <a
         onClick={handleABSwitch}
         className="fixed text-center bottom-24 w-48 z-50 bg-indigo-600 hover:bg-indigo-700 rounded text-white  p-2 text-large cursor-pointer left-10"
@@ -60,12 +60,15 @@ const Home = ({ products, posts, message, currency }) => {
       >
         Clear Cookies
       </a>
-      <h1
-        className="text-3xl font-bold tracking-tight px-20 py-10  shadow border-b"
-        id="personalBanner"
-      >
-        {message}
-      </h1>
+      <div className=" text-white bg-gray-700  font-bold tracking-tight px-20 py-4 z-0  shadow-lg">
+        <h1 className="text-3xl  mx-auto max-w-7xl  px-10" id="personalBanner">
+          {message}
+        </h1>
+        <h2 className="text-gray-300 text-2xl mx-auto max-w-7xl  px-10">
+          This is your personalised experience
+        </h2>
+      </div>
+
       <div className="relative b px-4 pt-16 pb-20 sm:px-6 lg:px-8 lg:pt-24 lg:pb-28">
         <div className="absolute inset-0">
           <div className="h-1/3  sm:h-2/3" />
@@ -75,7 +78,7 @@ const Home = ({ products, posts, message, currency }) => {
             <h2 className="text-3xl font-bold tracking-tight sm:text-4xl">
               From the blog
             </h2>
-            <p className="mx-auto mt-3 max-w-2xl text-xl text-gray-500 sm:mt-4">
+            <p className="mx-auto mt-3 max-w-2xl text-xl text-gray-300 sm:mt-4">
               Check out these personalised blog posts created just for you!
             </p>
           </div>
@@ -100,7 +103,7 @@ const Home = ({ products, posts, message, currency }) => {
                   <div className="flex-1">
                     <a href={post?.href} className="mt-2 block">
                       <p className="text-xl font-semibold text-gray-900">
-                        {post?.title}
+                        {post?.title + "  "}
                       </p>
                       <p className="mt-3 text-base text-gray-500">
                         {post?.description}
@@ -114,30 +117,48 @@ const Home = ({ products, posts, message, currency }) => {
         </div>
       </div>
 
-      <div className="h-20 bg-white"></div>
+      <div className="">
+        <img
+          className="h-72 object-cover w-full"
+          src="https://images.unsplash.com/photo-1665686308827-eb62e4f6604d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80"
+        />
+      </div>
+
+      <div className="relative mx-auto max-w-7xl pt-20">
+        <div className="text-center">
+          <h2 className="text-3xl font-bold tracking-tight sm:text-4xl">
+            Products just for you
+          </h2>
+          <p className="mx-auto mt-3 max-w-2xl text-xl text-gray-300 sm:mt-4">
+            Check out these awesome items based on your prefrences
+          </p>
+        </div>
+      </div>
       <div className="mx-auto max-w-7xl overflow-hidden py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
         <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 lg:gap-x-8">
           {hydratedProducts.map((product) => (
             <a
               key={product?.title}
               href={product?.link}
-              className="group text-sm bg-white shadow-lg h-72  hover:scale-105 transition-all duration-200 rounded-lg flex justify-between flex-col"
+              className="group text-sm bg-white shadow-lg   hover:scale-105 transition-all duration-200 rounded-lg flex justify-between flex-col"
             >
               <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden h-46 rounded-lg bg-gray-100 group-hover:opacity-75">
                 <img
                   src={product?.thumbnail}
                   alt="Not Found"
-                  className="h-46 w-full object-cover object-center"
+                  className="h-48 w-full object-cover object-center"
                 />
               </div>
-              <h3 className="mt-4 font-medium text-gray-900">
-                {product.title}
-              </h3>
+              <div className="px-4 py-2">
+                <h3 className="mt-4 h-8 font-medium text-gray-900">
+                  {product.title}
+                </h3>
 
-              <p className="mt-2 font-medium text-gray-900">
-                <span id="currency">{currency}</span>
-                {product?.extracted_price.toFixed(2)}
-              </p>
+                <p className="pt-4 font-medium text-xl text-gray-900">
+                  <span id="currency">{currency}</span>
+                  {product?.extracted_price.toFixed(2)}
+                </p>
+              </div>
             </a>
           ))}
         </div>
